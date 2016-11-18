40347465ae105f0e91daa3170cca6c39be0a2f67
stefan <stefan.wayand@cern.ch>
2016-11-18 17:05:30 +0100

    restructering of Prepare_all.py, now it is clearer again

--- Prepare_all.py
+++ Prepare_all.py
@@ -1,83 +1,92 @@
 import os,stat
 
 class finale_state():
-	def __init__(self, name, selection=False, generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G'], inputfolder = "Run2016_CMSSW_8_0_21", add_dbs=None ):
-		self.selection=selection
+	def __init__(self, name, generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G'], add_dbs=None, inputfolder = "Run2016_CMSSW_8_0_21"):
 		self.name = name
-		self.runs = runs
+		self.gc_cfgs = [] 
 		self.inputfolder = inputfolder
-		self.generator_base = 'generator.py'
-		self.files_to_copy = ['lheprodandcleaning.py','generator.py','merging.py','grid_control_fullembedding_data_base.conf']
-		if self.selection:
-			self.files_to_copy.append('selection.py')
+		self.cmsRun_order =  ['preselection.py','selection.py','lheprodandcleaning.py','generator.py','merging.py'] ## will be overwitten by copy_pyconfigs (checks is exsist in input folder)
 		if not os.path.exists(self.name):
 			os.mkdir(self.name)
-		for add_run in self.runs:
-			self.files_to_copy.append(add_run+'.dbs')
+		self.copy_pyconfigs(generator_frag=generator_frag)
+		self.copy_gcconfigs(runs=runs,add_dbs=add_dbs)
+		self.write_while()
+	def copy_pyconfigs(self, generator_frag=""):
+		is_first = True ## can be late used to init the first config with customize_for_gc and the later with the dummy stings 
+		self.cmsRun_order = []
+		for file_to_copy in ['preselection.py','selection.py','lheprodandcleaning.py','generator.py','merging.py']:
+		        add_fragment_to_end = ""
+			if file_to_copy == 'generator.py':
+				add_fragment_to_end=generator_frag
+			if self.copy_file(file_to_copy, add_fragment_to_end=add_fragment_to_end, skip_if_not_there=True):
+				is_first = False
+				self.cmsRun_order.append(file_to_copy)
+	def copy_gcconfigs(self, runs=[], add_dbs=None):
+	  	for add_run in runs:
+			self.copy_file(add_run+'.dbs', copy_from_folder = 'dbs')
 			if not os.path.exists(self.name+'/'+add_run+'.conf'):
 				self.write_cfg(add_run=add_run)
 		if add_dbs:
-		  self.write_cfg(add_dbs=add_dbs)
-		for file_to_copy in self.files_to_copy:
-			if os.path.exists(self.name+'/'+file_to_copy):
-				continue
-			if file_to_copy == 'generator.py':
-				self.copy_file(file_to_copy, add_fragment_to_end=generator_frag, copy_from_folder = self.inputfolder)
-			elif file_to_copy == 'lheprodandcleaning.py':
-				if self.selection:
-					self.copy_file(file_to_copy, copy_from_folder = self.inputfolder)
-				else:
-					self.copy_file(file_to_copy, add_fragment_to_end='process = customise_for_gc(process)', copy_from_folder = self.inputfolder)
-			elif file_to_copy in ['merging.py','selection.py']:
-				self.copy_file(file_to_copy, copy_from_folder = self.inputfolder)
-			elif file_to_copy.endswith('.dbs'):
-				self.copy_file(file_to_copy, copy_from_folder = 'dbs')
-			else:
-				self.copy_file(file_to_copy)
-		if not os.path.exists(self.name+'/while.sh'+file_to_copy):
-			gc_configs_base = self.runs
-			if add_dbs:
-			  gc_configs_base.append('DAS')
-			self.write_while(gc_configs_base)
-	def copy_file(self, in_file_name, copy_from_folder = './' ,add_fragment_to_end=""):
-			in_file = open (copy_from_folder.rstrip('/')+'/'+in_file_name, 'r')
-			file_str = in_file.read()
-			in_file.close()
-			file_str += add_fragment_to_end
-			out_file = open(self.name+'/'+in_file_name,'w')
-			out_file.write(file_str)
-			out_file.close()
+			self.write_cfg(add_dbs=add_dbs)	
+		cmsRun_order_str = 'config file = '
+		for cmsRun_cfg in self.cmsRun_order:
+			cmsRun_order_str += cmsRun_cfg+'\n\t\t'
+		rp_base_cfg = {}
+		rp_base_cfg['__CMSRUN_ORDER__'] = cmsRun_order_str
+		self.copy_file('grid_control_fullembedding_data_base.conf', copy_from_folder='./' ,repalce_dict=rp_base_cfg)
+		 
+		
+	def copy_file(self, in_file_name, copy_from_folder = None ,add_fragment_to_end="", skip_if_not_there=False, overwrite=False, repalce_dict={}):
+		if not copy_from_folder:
+			copy_from_folder = self.inputfolder
+		if skip_if_not_there and not os.path.isfile(copy_from_folder.rstrip('/')+'/'+in_file_name):
+			return False
+		in_file = open (copy_from_folder.rstrip('/')+'/'+in_file_name, 'r')
+		file_str = in_file.read()
+		in_file.close()
+		if os.path.isfile(self.name+'/'+in_file_name) and not overwrite: ## do not overwrit if the file exists
+			return True
+		file_str += add_fragment_to_end
+		for replace in repalce_dict: ## replace Variable by the value. 
+			file_str = file_str.replace(replace,repalce_dict[replace])
+		out_file = open(self.name+'/'+in_file_name,'w')
+		out_file.write(file_str)
+		out_file.close()
+		return True
 	def write_cfg(self, add_run=None, add_dbs=None):
-			try:
-			  out_file = open(self.name+'/'+add_run+'.conf','w')
-			except:
-			  out_file = open(self.name+'/DAS.conf','w')
-			out_file.write('[global]\n')
-			out_file.write('include=grid_control_fullembedding_data_base.conf\n')
-			out_file.write('workdir = /portal/ekpbms2/home/${USER}/embedding/gc_workdir/TauEmbedding_'+out_file.name.split('.')[0]+'\n')
-			out_file.write('[CMSSW]\n')
-			if add_run:
-			  out_file.write('dataset = TauEmbedding_'+self.name+'_'+add_run+' :  list:'+add_run+'.dbs\n')
-			if add_dbs:
-			   for akt_name in add_dbs:
-			      out_file.write('dataset = TauEmbedding_'+self.name+'_'+akt_name+' :  dbs:'+add_dbs[akt_name]+'\n')
-			out_file.close()
-	def write_while(self, all_runs=[]):
-			out_file = open(self.name+'/while.sh','w')
-			out_file.write('#!/bin/bash\n')
-			out_file.write('\n')
-			out_file.write('touch .lock\n')
-			out_file.write('\n')
-			out_file.write('while [ -f ".lock" ]\n')
-			out_file.write('do\n')
-			for add_run in all_runs:
-				out_file.write('go.py '+add_run+'.conf -G \n')
-			out_file.write('echo "rm .lock"\n')
-			out_file.write('sleep 2\n')
-			out_file.write('done\n')
-			out_file.close()
-			os.chmod(self.name+'/while.sh',stat.S_IRWXU)
-			
+		try:
+			out_file = open(self.name+'/'+add_run+'.conf','w')
+		except:
+			out_file = open(self.name+'/DAS.conf','w')
+		out_file.write('[global]\n')
+		out_file.write('include=grid_control_fullembedding_data_base.conf\n')
+		out_file.write('workdir = /portal/ekpbms2/home/${USER}/embedding/gc_workdir/TauEmbedding_'+out_file.name.split('.')[0]+'\n')
+		out_file.write('[CMSSW]\n')
+		if add_run:
+			out_file.write('dataset = TauEmbedding_'+self.name+'_'+add_run+' :  list:'+add_run+'.dbs\n')
+		if add_dbs:
+			for akt_name in add_dbs:
+				out_file.write('dataset = TauEmbedding_'+self.name+'_'+akt_name+' :  dbs:'+add_dbs[akt_name]+'\n')
+		out_file.close()
+		self.gc_cfgs.append(out_file.name) ## save the 
+		
+	def write_while(self, overwrite=False):
+	        if os.path.isfile(self.name+'/while.sh') and not overwrite:
+			return
+		out_file = open(self.name+'/while.sh','w')
+		out_file.write('#!/bin/bash\n')
+		out_file.write('\n')
+		out_file.write('touch .lock\n')
+		out_file.write('\n')
+		out_file.write('while [ -f ".lock" ]\n')
+		out_file.write('do\n')
+		for akt_cfg in self.gc_cfgs:
+			out_file.write('go.py '+akt_cfg+' -G \n')
+		out_file.write('echo "rm .lock"\n')
+		out_file.write('sleep 2\n')
+		out_file.write('done\n')
+		out_file.close()
+		os.chmod(self.name+'/while.sh',stat.S_IRWXU)
 
 
 
@@ -94,7 +103,7 @@ dbs_map["DYJetsToLLM50_FlatPU28to62"]="/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgr
 
 
 for akt_final_states in generator_frag_map:
-  finale_state(name=akt_final_states+'rmc2016_v1', selection=True, runs=[], inputfolder="MC2016_CMSSW_8_0_21" ,generator_frag = generator_frag_map[akt_final_states],add_dbs=dbs_map)
+  finale_state(name=akt_final_states+'rmc2016_v1', runs=[], inputfolder="MC2016_CMSSW_8_0_21" ,generator_frag = generator_frag_map[akt_final_states],add_dbs=dbs_map)
 
 to_process = []
 
