b7973eb0704ded07d6e1c223000f2470c348e9f3
janekbechtel <janek.bechtel@student.kit.edu>
2016-11-22 11:53:07 +0100

    Create Prepare_MC and change se output pattern depending on final state

--- Prepare_all.py
+++ Prepare_all.py
@@ -1,14 +1,17 @@
 import os,stat
 
 class finale_state():
-	def __init__(self, name, generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G'], add_dbs=None, inputfolder = "Run2016_CMSSW_8_0_21"):
-		self.name = name
+	def __init__(self, finalstate, identifier="", generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G','Run2016H'], add_dbs=None, inputfolder = "Run2016_CMSSW_8_0_21"):
+		self.finalstate=finalstate
+		self.identifier=identifier
+		self.name = finalstate+"_"+identifier
 		self.gc_cfgs = [] 
 		self.inputfolder = inputfolder
 		self.cmsRun_order =  ['preselection.py','selection.py','lheprodandcleaning.py','generator.py','merging.py'] ## will be overwitten by copy_pyconfigs (checks is exsist in input folder)
 		if not os.path.exists(self.name):
 			os.mkdir(self.name)
-		self.copy_pyconfigs(generator_frag=generator_frag)
+		generator_frag_map=self.make_generator_frag_map(this_finalstate=self.finalstate)
+		self.copy_pyconfigs(generator_frag=generator_frag_map[self.finalstate])
 		self.copy_gcconfigs(runs=runs,add_dbs=add_dbs)
 		self.write_while()
 	def copy_pyconfigs(self, generator_frag=""):
@@ -33,10 +36,12 @@ class finale_state():
 			cmsRun_order_str += cmsRun_cfg+'\n\t\t'
 		rp_base_cfg = {}
 		rp_base_cfg['__CMSRUN_ORDER__'] = cmsRun_order_str
-		self.copy_file('grid_control_fullembedding_data_base.conf', copy_from_folder='./' ,repalce_dict=rp_base_cfg)
-		 
+		se_output_pattern_str= 'se output pattern = '+self.finalstate+'_fullembedding_'+self.identifier+'_'+self.inputfolder+'_freiburg/@NICK@/@FOLDER@/@XBASE@_@GC_JOB_ID@.@XEXT@'
+		rp_base_cfg['__SE_OUTPUT_PATTERN__']=se_output_pattern_str
+		self.copy_file('grid_control_fullembedding_data_base.conf', copy_from_folder='./' ,replace_dict=rp_base_cfg)
 		
-	def copy_file(self, in_file_name, copy_from_folder = None ,add_fragment_to_end="", skip_if_not_there=False, overwrite=False, repalce_dict={}):
+		
+	def copy_file(self, in_file_name, copy_from_folder = None ,add_fragment_to_end="", skip_if_not_there=False, overwrite=False, replace_dict={}):
 		if not copy_from_folder:
 			copy_from_folder = self.inputfolder
 		if skip_if_not_there and not os.path.isfile(copy_from_folder.rstrip('/')+'/'+in_file_name):
@@ -47,8 +52,8 @@ class finale_state():
 		if os.path.isfile(self.name+'/'+in_file_name) and not overwrite: ## do not overwrit if the file exists
 			return True
 		file_str += add_fragment_to_end
-		for replace in repalce_dict: ## replace Variable by the value. 
-			file_str = file_str.replace(replace,repalce_dict[replace])
+		for replace in replace_dict: ## replace Variable by the value. 
+			file_str = file_str.replace(replace,replace_dict[replace])
 		out_file = open(self.name+'/'+in_file_name,'w')
 		out_file.write(file_str)
 		out_file.close()
@@ -87,23 +92,22 @@ class finale_state():
 		out_file.write('done\n')
 		out_file.close()
 		os.chmod(self.name+'/while.sh',stat.S_IRWXU)
+	def make_generator_frag_map(self,this_finalstate):
+		generator_frag_map = {}
+		if this_finalstate=="MuTau":
+			generator_frag_map["MuTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(MuHadCut = cms.untracked.string("Mu.Pt > 18 && Had.Pt > 18 && Mu.Eta < 2.2 && Had.Eta < 2.4"))'
+		elif this_finalstate=="ElTau":
+			generator_frag_map["ElTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElHadCut = cms.untracked.string("El.Pt > 23 && Had.Pt > 18 && El.Eta < 2.2 && Had.Eta < 2.4 "))'
+		elif this_finalstate=="ElMu":
+			generator_frag_map["ElMu"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElMuCut = cms.untracked.string("(El.Pt > 16 && Mu.Pt > 8) || (El.Pt > 11 && Mu.Pt > 16)"))'
+		elif this_finalstate=="TauTau":
+			generator_frag_map["TauTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(HadHadCut = cms.untracked.string("Had1.Pt > 38 && Had2.Pt > 38 && Had1.Eta < 2.2 && Had2.Eta < 2.2"))' 
+		return generator_frag_map
 
+final_states=["MuTau","ElTau","ElMu","TauTau"]
 
-
-
-generator_frag_map = {}
-
-generator_frag_map["MuTau_"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(MuHadCut = cms.untracked.string("Mu.Pt > 18 && Had.Pt > 18 && Mu.Eta < 2.2 && Had.Eta < 2.4"))'
-generator_frag_map["ElTau_"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElHadCut = cms.untracked.string("El.Pt > 23 && Had.Pt > 18 && El.Eta < 2.2 && Had.Eta < 2.4 "))'
-generator_frag_map["ElMu_"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElMuCut = cms.untracked.string("(El.Pt > 16 && Mu.Pt > 8) || (El.Pt > 11 && Mu.Pt > 16)"))'
-generator_frag_map["TauTau_"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(HadHadCut = cms.untracked.string("Had1.Pt > 38 && Had2.Pt > 38 && Had1.Eta < 2.2 && Had2.Eta < 2.2"))' 
-
-dbs_map = {}
-dbs_map["DYJetsToLLM50_FlatPU28to62"]="/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80-FlatPU28to62HcalNZSRAWAODSIM_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/RAWAODSIM"
-
-
-for akt_final_states in generator_frag_map:
-  finale_state(name=akt_final_states+'rmc2016_v1', runs=[], inputfolder="MC2016_CMSSW_8_0_21" ,generator_frag = generator_frag_map[akt_final_states],add_dbs=dbs_map)
+for finalstate in final_states:
+  finale_state(finalstate=finalstate, identifier="data", runs=["Run2016B","Run2016C","Run2016D","Run2016E","Run2016F","Run2016G","Run2016H"], inputfolder="Run2016_CMSSW_8_0_21", add_dbs=None)
 
 to_process = []
 
