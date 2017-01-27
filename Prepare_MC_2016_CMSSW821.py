0c04541c42738745ac87ec167132644886ae9ae9
janekbechtel <janek.bechtel@student.kit.edu>
2017-01-27 12:10:33 +0100

    Update Prepare scripts and gc config

--- Prepare_MC_2016_CMSSW821.py
+++ Prepare_MC_2016_CMSSW821.py
@@ -2,10 +2,17 @@ from Prepare_all import finale_state
 
 
 if __name__ == "__main__":
-	final_states=["MuTau","ElTau","ElMu","TauTau"]
-
+	#final_states=["MuTau","ElTau","ElMu","TauTau"]
+	final_states=["TauTau","ElMu"]
 	dbs_map = {}
 	dbs_map["DYJetsToLLM50_FlatPU28to62"]="/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16DR80-FlatPU28to62HcalNZSRAWAODSIM_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/RAWAODSIM"
 
+	generator_frag_map = {}
+	generator_frag_map["MuTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(MuHadCut = cms.untracked.string("Mu.Pt > 8 && Had.Pt > 18 && Mu.Eta < 2.2 && Had.Eta < 2.4"))'
+	generator_frag_map["ElTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElHadCut = cms.untracked.string("El.Pt > 13 && Had.Pt > 18 && El.Eta < 2.2 && Had.Eta < 2.4 "))'
+	generator_frag_map["ElMu"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElMuCut = cms.untracked.string("(El.Pt > 13 && Mu.Pt > 8)"))'
+	generator_frag_map["TauTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(HadHadCut = cms.untracked.string("Had1.Pt > 38 && Had2.Pt > 38 && Had1.Eta < 2.2 && Had2.Eta < 2.2"))' 
+
+
 	for finalstate in final_states:
-		finale_state(finalstate=finalstate, identifier="MC_2016_CMSSW821",runs=[], inputfolder="MC2016_CMSSW_8_0_21", add_dbs=dbs_map)
+		finale_state(finalstate=finalstate, identifier="MC_2016_CMSSW821",runs=[], inputfolder="MC2016_CMSSW_8_0_21", add_dbs=dbs_map, generator_frag_map=generator_frag_map)
--- Prepare_all.py
+++ Prepare_all.py
@@ -1,17 +1,20 @@
 import os,stat
 
 class finale_state():
-	def __init__(self, finalstate, identifier="", generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G','Run2016H'], add_dbs=None, inputfolder = "Run2016_CMSSW_8_0_21"):
+	def __init__(self, finalstate, identifier="", generator_frag="", runs = ['Run2016B','Run2016C','Run2016D','Run2016E','Run2016F','Run2016G','Run2016H'], add_dbs=None, inputfolder = "Run2016_CMSSW_8_0_21",generator_frag_map=None):
 		self.finalstate=finalstate
 		self.identifier=identifier
 		self.name = finalstate+"_"+identifier
-		self.gc_cfgs = [] 
+		self.gc_cfgs = []
+		self.generator_frag_map = generator_frag_map
 		self.inputfolder = inputfolder
 		self.cmsRun_order =  ['preselection.py','selection.py','lheprodandcleaning.py','generator.py','merging.py'] ## will be overwitten by copy_pyconfigs (checks is exsist in input folder)
 		if not os.path.exists(self.name):
 			os.mkdir(self.name)
-		generator_frag_map=self.make_generator_frag_map(this_finalstate=self.finalstate)
-		self.copy_pyconfigs(generator_frag=generator_frag_map[self.finalstate])
+		if self.generator_frag_map is None:
+			self.generator_frag_map=self.make_generator_frag_map(this_finalstate=self.finalstate)
+			
+		self.copy_pyconfigs(generator_frag=self.generator_frag_map[self.finalstate])
 		self.copy_gcconfigs(runs=runs,add_dbs=add_dbs)
 		self.write_while()
 	def copy_pyconfigs(self, generator_frag=""):
@@ -38,7 +41,7 @@ class finale_state():
 		rp_base_cfg['__CMSRUN_ORDER__'] = cmsRun_order_str
 		se_path_str = 'se path = srm://dcache-se-cms.desy.de:8443/srm/managerv2?SFN=/pnfs/desy.de/cms/tier2/store/user/'+os.environ["USER"]+'/gc_storage'
 		rp_base_cfg['__SE_PATH__']=se_path_str
-		se_output_pattern_str= 'se output pattern = '+self.finalstate+'_fullembedding_'+self.identifier+'_freiburg/@NICK@/@FOLDER@/@XBASE@_@GC_JOB_ID@.@XEXT@'
+		se_output_pattern_str= 'se output pattern = '+self.finalstate+'_'+self.identifier+'_freiburg/@NICK@/@FOLDER@/@XBASE@_@GC_JOB_ID@.@XEXT@'
 		rp_base_cfg['__SE_OUTPUT_PATTERN__']=se_output_pattern_str
 		self.copy_file('grid_control_fullembedding_data_base.conf', copy_from_folder='./' ,replace_dict=rp_base_cfg)
 		
@@ -94,14 +97,15 @@ class finale_state():
 		out_file.write('done\n')
 		out_file.close()
 		os.chmod(self.name+'/while.sh',stat.S_IRWXU)
-	def make_generator_frag_map(self,this_finalstate):
-		generator_frag_map = {}
-		if this_finalstate=="MuTau":
-			generator_frag_map["MuTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(MuHadCut = cms.untracked.string("Mu.Pt > 18 && Had.Pt > 18 && Mu.Eta < 2.2 && Had.Eta < 2.4"))'
-		elif this_finalstate=="ElTau":
-			generator_frag_map["ElTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElHadCut = cms.untracked.string("El.Pt > 23 && Had.Pt > 18 && El.Eta < 2.2 && Had.Eta < 2.4 "))'
-		elif this_finalstate=="ElMu":
-			generator_frag_map["ElMu"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElMuCut = cms.untracked.string("(El.Pt > 16 && Mu.Pt > 8) || (El.Pt > 11 && Mu.Pt > 16)"))'
-		elif this_finalstate=="TauTau":
-			generator_frag_map["TauTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(HadHadCut = cms.untracked.string("Had1.Pt > 38 && Had2.Pt > 38 && Had1.Eta < 2.2 && Had2.Eta < 2.2"))' 
+	def make_generator_frag_map(self,this_finalstate,generator_frag_map=None):
+		if generator_frag_map is None:
+			generator_frag_map = {}
+			if this_finalstate=="MuTau":
+				generator_frag_map["MuTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(MuHadCut = cms.untracked.string("Mu.Pt > 18 && Had.Pt > 18 && Mu.Eta < 2.2 && Had.Eta < 2.4"))'
+			elif this_finalstate=="ElTau":
+				generator_frag_map["ElTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElHadCut = cms.untracked.string("El.Pt > 23 && Had.Pt > 18 && El.Eta < 2.2 && Had.Eta < 2.4 "))'
+			elif this_finalstate=="ElMu":
+				generator_frag_map["ElMu"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(ElMuCut = cms.untracked.string("(El.Pt > 16 && Mu.Pt > 8) || (El.Pt > 11 && Mu.Pt > 16)"))'
+			elif this_finalstate=="TauTau":
+				generator_frag_map["TauTau"] = 'process.generator.HepMCFilter.filterParameters = cms.PSet(HadHadCut = cms.untracked.string("Had1.Pt > 38 && Had2.Pt > 38 && Had1.Eta < 2.2 && Had2.Eta < 2.2"))' 
 		return generator_frag_map
