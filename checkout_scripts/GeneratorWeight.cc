f437c42b27e65dab4f2f5bfb8042405279de323b
janekbechtel <janek.bechtel@student.kit.edu>
2017-06-29 11:12:50 +0200

    Update checkout scripts

--- checkout_scripts/GeneratorWeight.cc
+++ checkout_scripts/GeneratorWeight.cc
@@ -7,9 +7,9 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 
-class Simple_Plots : public edm::one::EDAnalyzer<>  {
+class GeneratorWeight : public edm::one::EDAnalyzer<>  {
    public:
-      explicit Simple_Plots(const edm::ParameterSet&);
+      explicit GeneratorWeight(const edm::ParameterSet&);
    private:
       virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
       edm::EDGetTokenT<GenEventInfoProduct> MyGenEventInfoProduct_;
@@ -17,14 +17,14 @@ class Simple_Plots : public edm::one::EDAnalyzer<>  {
 
 
 //
-Simple_Plots::Simple_Plots(const edm::ParameterSet& iConfig){  
+GeneratorWeight::GeneratorWeight(const edm::ParameterSet& iConfig){  
   //MyGenEventInfoProduct_ = consumes<GenEventInfoProduct>( iConfig.getParameter<edm::InputTag>("genSource") );
   MyGenEventInfoProduct_ = consumes<GenEventInfoProduct>( edm::InputTag("generator") );}
 
 // ------------ method called for each event  ------------
 void
-Simple_Plots::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
+GeneratorWeight::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup){
   edm::Handle<GenEventInfoProduct> GenEventInfo;
   iEvent.getByToken(MyGenEventInfoProduct_, GenEventInfo);
   std::cout << GenEventInfo->weight() << std::endl;}
-DEFINE_FWK_MODULE(Simple_Plots);
+DEFINE_FWK_MODULE(GeneratorWeight);
