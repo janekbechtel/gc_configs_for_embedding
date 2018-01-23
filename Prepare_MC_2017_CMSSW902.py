c9d2cfbdc5fdb4c9c07edf0d1564957f948d5e75
Per Ahrens <per.ahrens@gmx.de>
2018-01-23 22:09:34 +0100

    Fixed prepare script for MC

--- Prepare_MC_2017_CMSSW902.py
+++ Prepare_MC_2017_CMSSW902.py
@@ -2,5 +2,9 @@ from Prepare_all import finale_state
 
 if __name__ == "__main__":
 	final_states=["MuTau","ElTau","ElMu","TauTau","MuEmb","ElEmb"]
+
+	dbs_map = {}
+	dbs_map["DYJetsToLLM50_FlatPU28to62"]="/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/PhaseISpring17DR-FlatPU28to62HcalNZS_90X_upgrade2017_realistic_v20-v1/GEN-SIM-RAW"
+
 	for finalstate in final_states:
-		finale_state(finalstate=finalstate, identifier="MC_2017_CMSSW902", runs=["Run2017B","Run2017C","Run2017D","Run2017E","Run2017F"], inputfolder="MC2017_CMSSW_9_0_2", add_dbs=None)
+		finale_state(finalstate=finalstate, identifier="MC_2017_CMSSW902", runs=[], inputfolder="MC2017_CMSSW_9_0_2", add_dbs=dbs_map)
